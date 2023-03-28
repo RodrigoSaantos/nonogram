f8873fadad38db906d434367dc2c1a93d244cbe6
Rodrigo Santos <rodrigo.silva@gcbinvestimentos.com>
2023-03-28 19:36:07 -0300

    chore: add modal completed puzzle

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -1,5 +1,5 @@
 import { useControl } from "@/hooks/useControl";
-import { Button, Flex, Modal, ModalBody, ModalContent, ModalHeader, useToken } from "@chakra-ui/react";
+import { Box, Button, Flex, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useToken } from "@chakra-ui/react";
 import { AiFillHeart } from 'react-icons/ai';
 import { Canvas } from "../Canvas";
 import { Control } from "../Control";
@@ -19,7 +19,7 @@ const Hearts = () => {
 };
 
 export function Screen() {
-  const { lives, onReset } = useControl()
+  const { lives, onReset, completedPuzzle, board } = useControl()
   const [red500] = useToken('colors', ['red.500'])
   return (
     <Flex flexDirection={'column'} gap="8" width={'100%'} height="100vh" justify={'center'} align='center'>
@@ -37,6 +37,32 @@ export function Screen() {
               <Button colorScheme={'blue'} onClick={onReset}>Recomeçar</Button>
             </Flex>
           </ModalBody>
+        </ModalContent>
+      </Modal>
+      <Modal isCentered isOpen={completedPuzzle} onClose={() => null}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader textAlign={'center'}>
+            Nível completado!
+          </ModalHeader>
+          <ModalBody>
+            <Flex flexDir={'column'} gap={8} justify={'center'} alignItems='center'>
+              <Box
+                display={'grid'}
+                gridTemplateColumns="repeat(10, 1fr)"
+                border='2px solid'
+              >
+                {board.map((grid, index) => {
+                  return (
+                    <Box key={index} w={4} h={4} background={grid ? 'gray.500' : 'transparent'} />
+                  )
+                })}
+              </Box>
+            </Flex>
+          </ModalBody>
+          <ModalFooter justifyContent={'center'}>
+            <Button colorScheme={'blue'} onClick={onReset}>Próximo Nível</Button>
+          </ModalFooter>
         </ModalContent>
       </Modal>
     </Flex>
